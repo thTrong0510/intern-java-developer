dd852226a77044d8c3666875cd92f04838d5744f
thTrong0510 <vthanhtrongng@gmail.com>
2025-09-10 20:17:56 +0700

    init project + connect to mysql successfully

---
+++ src/main/java/com/trong/blog/blog/controller/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.trong.blog.blog.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+	@GetMapping("/")
+	public String helloWorld() {
+		return "Hello World";
+	}
+}
